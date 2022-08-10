fde637ccedc8d22ab705be19d866a699a5c9ee72
Jonas Moreau <jonas.moreau@hotmail.com>
2022-08-10 15:36:20 +0200

    architecture - setup de base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRoutes } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let element = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/Kalevala", element: <Kalevala /> },
+    { path: "/shop", element: <ShopList /> },
+    { path: "/shop/:id", element: <Shop /> },
+    { path: "/events", element: <EventList /> },
+    { path: "/events/:id", element: <Event /> },
+    { path: "/about", element: <About /> },
+  ]);
+  return <div className="App">{element}</div>;
 }
 
 export default App;
