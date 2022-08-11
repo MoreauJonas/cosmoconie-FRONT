6a42ca83d57f460fb272a57f2857efe1a25773d8
Jonas Moreau <jonas.moreau@hotmail.com>
2022-08-11 02:49:15 +0200

    add map event + add filter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { useRoutes } from "react-router-dom";
+import EventList from "../src/screens/EventList.jsx";
 import "./App.css";
 
 function App() {
   let element = useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "/Kalevala", element: <Kalevala /> },
-    { path: "/shop", element: <ShopList /> },
-    { path: "/shop/:id", element: <Shop /> },
+    // { path: "/", element: <Home /> },
+    // { path: "/Kalevala", element: <Kalevala /> },
+    // { path: "/shop", element: <ShopList /> },
+    // { path: "/shop/:id", element: <Shop /> },
     { path: "/events", element: <EventList /> },
-    { path: "/events/:id", element: <Event /> },
-    { path: "/about", element: <About /> },
+    // { path: "/events/:id", element: <Event /> },
+    // { path: "/about", element: <About /> },
   ]);
   return <div className="App">{element}</div>;
 }
