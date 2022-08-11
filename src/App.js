48b0321a11537e04fbebf6ae0ac665df7b5b1d38
Fortineau Laetitia <laetitia.fortineau@gmail.com>
2022-08-11 20:51:13 +0200

    ajout about

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { useRoutes } from "react-router-dom";
 import "./App.css";
+import About from "./screens/About";
 
 function App() {
   let element = useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "/Kalevala", element: <Kalevala /> },
-    { path: "/shop", element: <ShopList /> },
-    { path: "/shop/:id", element: <Shop /> },
-    { path: "/events", element: <EventList /> },
-    { path: "/events/:id", element: <Event /> },
+    // { path: "/", element: <Home /> },
+    // { path: "/Kalevala", element: <Kalevala /> },
+    // { path: "/shop", element: <ShopList /> },
+    // { path: "/shop/:id", element: <Shop /> },
+    // { path: "/events", element: <EventList /> },
+    // { path: "/events/:id", element: <Event /> },
     { path: "/about", element: <About /> },
   ]);
   return <div className="App">{element}</div>;
