22fe6029217949973d95d72b92a0102ef86a6f9f
REMI SAMSON <samson.rem@gmail.com>
2022-08-11 19:46:46 +0300

    ready navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,26 @@
+import { NavLink } from 'react-router-dom';
+import * as Icon from 'react-feather';
+import cosmoconieLogo from '../assets/images/logo1.png';
+
 const Navbar = () => {
     return (
-        <div className="navbar"></div>
-    )
+        <div className="navbar">
+            <NavLink to="/" ><img src={cosmoconieLogo} alt="logo cosmoconie"/>
+            </NavLink>
+            <NavLink to='/Kalevala' className={({ isActive }) => (isActive ? 'active' : undefined)}>
+                <div><Icon.Feather size={40} />Kalevala</div>
+            </NavLink>
+            <NavLink to='/shop' className={({ isActive }) => (isActive ? 'active' : undefined)}>
+                <div><Icon.ShoppingBag size={40} />Shopping</div>
+            </NavLink>
+            <NavLink to='/events' className={({ isActive }) => (isActive ? 'active' : undefined)}>
+                <div><Icon.Globe size={40}/>Events</div>
+            </NavLink>
+            <NavLink to='/about' className={({ isActive }) => (isActive ? 'active' : undefined)}>
+                <div><Icon.Smile size={40} />About us</div>
+            </NavLink>
+        </div>
+    );
 }
 
 export default Navbar
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,9 @@
 import Navbar from '../components/Navbar'
-import Header from '../components/Header'
 
 const Home = () => {
     return (
         <div className="home">
             <Navbar />
-            <Header />
         </div>
     )
 }
