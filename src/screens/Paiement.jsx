23f9667feb7b7c2d3858dc003b058ea471764664
Joshua <joshua35260@gmail.com>
2022-08-12 10:09:34 +0200

    ester egg david

--- src/screens/Paiement.jsx
+++ src/screens/Paiement.jsx
@@ -1,7 +1,8 @@
+import paiement from '../assets/images/paiement.png'
 function Paiement() {
   return (
     <div className="cardlol">
-      <img src="https://cdn.discordapp.com/attachments/1004283893526044723/1007547446957703168/paiement_en_ligne.png"></img>
+      <img src={paiement}></img>
     </div>
   );
 }
